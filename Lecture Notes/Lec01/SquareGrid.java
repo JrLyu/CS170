926174b4a0447a1a2cbb37e43b2037446cadc5e2
Jiuru Lyu <lvjiuru@hotmail.com>
2023-01-18 17:14:23 -0500

    Update SquareGrid.java

--- Lecture Notes/Lec01/SquareGrid.java
+++ Lecture Notes/Lec01/SquareGrid.java
@@ -8,24 +8,24 @@ public class SquareGrid {
                 for (int j = 0; j < 5; j++) {
                     // Draw a square and a space of 60 steps
                     for (int i = 0; i < 4; i++) {
-                        bob.forward(40);
+                        bob.forward(40); // This code is run 4*5*3=60 times. 
                         bob.left(90);
                     }
                     bob.penup();
-                    bob.forward(100);
+                    bob.forward(100); // This code is run 5*3 = 15 times
                     bob.pendown();
                 }
                 bob.penup();
                 bob.backward(500);
                 bob.left(90);
-                bob.forward(50);
+                bob.forward(50); // This code is run three times. 
                 bob.right(90);
                 bob.pendown();
             }
             // Moving bob back the original direction and position
             bob.penup();
             bob.right(90); 
-            bob.forward(150);
+            bob.forward(150); // This code is run only once. 
             bob.left(90);
             bob.pendown();
         }
